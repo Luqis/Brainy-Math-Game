fa2d9ea744cc53b67434d2d46125fd1228ec2539
Luqis <luqmanidris@siswa.ukm.edu.my>
2016-10-11 23:34:18 +0800

    Renamed a few files

---
+++ Brainy Math Project/Assets/Script/ClickSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ClickSound1 : MonoBehaviour {
+	public AudioClip sound1;
+	private Button button { get { return GetComponent<Button> (); } }
+	private AudioSource source{get { return GetComponent<AudioSource> ();}}
+	// Use this for initialization
+	void Start () {
+
+		gameObject.AddComponent<AudioSource>();
+		source.clip=sound1;
+		source.playOnAwake = false;
+
+		button.onClick.AddListener (() => PlaySound ());
+	}
+	
+	// Update is called once per frame
+	void PlaySound () {
+		source.PlayOneShot (sound1);
+	}
+}
