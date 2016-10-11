eb87fb21d6ecc0deef3e2d598ba074f23814f767
Luqis <luqmanidris@siswa.ukm.edu.my>
2016-10-11 23:04:02 +0800

    Added Initial Project

---
+++ Brainy Math Project/Assets/Script/login.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class login : MonoBehaviour {
+	
+
+	public void changeScene(string sceneName)
+	{
+		Application.LoadLevel (sceneName);
+
+	}
+
+}
---
+++ Brainy Math Project/Assets/Script/loginscript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loginscript : MonoBehaviour {
+
+	public static string username = "";
+	public static string password = "";
+
+	string userUrl = "http://localhost/math/user.php";
+
+	public void getUsername (string getname){
+		username = getname;
+	}
+
+	public void getPassword (string getpass){
+		password= getpass;
+}
+
+	public void Login (){
+
+
+
+		StartCoroutine ("login");
+	}
+		
+
+		IEnumerator login(){
+
+			WWWForm form = new WWWForm ();
+			form.AddField ("usernamePost", username);
+			form.AddField ("passwordPost", password);
+			WWW	www = new WWW (userUrl, form);
+
+			yield return www;
+			Debug.Log (www.text);
+
+}
+	}	
